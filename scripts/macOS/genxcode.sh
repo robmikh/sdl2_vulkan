abcf2c5eb349fddecf2fceddd374859a62977f17
robmikh <rob.mikh@outlook.com>
2023-03-25 19:23:29 -0700

    fix script typo

--- scripts/macOS/genxcode.sh
+++ scripts/macOS/genxcode.sh
@@ -3,7 +3,7 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd)"
 
 REPO_ROOT_PATH="$(dirname $(dirname "$CURRENT_DIR"))"
-BUILDPATH="$REPO_ROOT_PATH\xcode"
+BUILDPATH="$REPO_ROOT_PATH/xcode"
 
 cmake . "-B$BUILDPATH" -GXcode
 open "$REPO_ROOT_PATH/xcode/sdl2_vulkan.xcodeproj"
